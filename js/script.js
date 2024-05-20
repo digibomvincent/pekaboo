ecc508bd3877892ac6ad9dbc0770729fce956c49
digibomvincent <digibomvincent@gmail.com>
2024-05-20 17:18:56 +0800

    0520

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
     var face1_box = document.getElementById("face1_box");
     face1_box.classList.add("hide");
 } */
+var iframe = document.getElementById('nav');
+iframe.onload = function () {
+    var iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+    var cartIcon = iframeDocument.getElementById('cartIcon');
+    cartIcon.addEventListener('click', function () {
+        window.toggleCart();
+    });
+};
